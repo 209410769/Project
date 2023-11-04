e1c0ae1653b13c91d570243c4bdd788325095b25
jimmylin <209410769@gms.tku.edu.tw>
2023-11-04 13:43:49 +0800

    delete DataBaseConnection

--- house-price-prediction/src/App.js
+++ house-price-prediction/src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
 import Rvr from './page/Rvr';
-import Test from './page/Test';
 import './App.css'
 
 function App() {
@@ -17,7 +16,6 @@ function App() {
         <Routes>
           <Route path="/" />
           <Route path="/rvr" element={<Rvr />} />
-          <Route path="/test" element={<Test />} />
         </Routes>
         <footer>
           &copy; TKU資工專題(實價登入)
--- house-price-prediction/src/page/Test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import DatabaseConnection from '../components/DatabaseConnection'
-function Test() {
-  return (
-    <div><DatabaseConnection/></div>
-  )
-}
-
-export default Test
